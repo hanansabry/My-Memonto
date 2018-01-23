f72f21a08be1db0c96a6770e628a75dbd624556f
Hanan Sabry <eng.hanan.sabry@gmail.com>
2018-01-23 21:03:47 +0200

    Add Floating action button and set its action to adding new category Adding swipe right and left action on recyclerview item

--- app/src/main/java/Utilites/SimpleCallbackTouchHelper.java
+++ app/src/main/java/Utilites/SimpleCallbackTouchHelper.java
@@ -4,7 +4,10 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.DisplayMetrics;
@@ -18,9 +21,12 @@ import learning.memento.toto.my_memonto.R;
 
 public abstract class SimpleCallbackTouchHelper extends ItemTouchHelper.SimpleCallback {
     public static final float ALPHA_FULL = 1.0f;
+    private Paint p = new Paint();
+    Context context;
 
-    public SimpleCallbackTouchHelper(int dragDirs, int swipeDirs) {
+    public SimpleCallbackTouchHelper(Context context, int dragDirs, int swipeDirs) {
         super(dragDirs, swipeDirs);
+        this.context = context;
     }
 
     @Override
@@ -28,65 +34,32 @@ public abstract class SimpleCallbackTouchHelper extends ItemTouchHelper.SimpleCa
         return false;
     }
 
-//    @Override
-//    public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
-//        if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
-//            // Get RecyclerView item from the ViewHolder
-//            View itemView = viewHolder.itemView;
-//
-//            Paint p = new Paint();
-//            Bitmap icon;
-//
-//            if (dX > 0) {
-//            /* Note, ApplicationManager is a helper class I created
-//               myself to get a context outside an Activity class -
-//               feel free to use your own method */
-//
-//                icon = BitmapFactory.decodeResource(
-//                        context.getResources(), android.R.drawable.ic_menu_delete);
-//
-//            /* Set your color for positive displacement */
-//                p.setARGB(255, 255, 0, 0);
-//
-//                // Draw Rect with varying right side, equal to displacement dX
-//                c.drawRect((float) itemView.getLeft(), (float) itemView.getTop(), dX,
-//                        (float) itemView.getBottom(), p);
-//
-//                // Set the image icon for Right swipe
-//                c.drawBitmap(icon,
-//                        (float) itemView.getLeft() + convertDpToPx(16),
-//                        (float) itemView.getTop() + ((float) itemView.getBottom() - (float) itemView.getTop() - icon.getHeight()) / 2,
-//                        p);
-//            }
-//            else {
-//                icon = BitmapFactory.decodeResource(
-//                        context.getResources(), android.R.drawable.arrow_down_float);
-//
-//            /* Set your color for negative displacement */
-//                p.setARGB(255, 0, 255, 0);
-//
-//                // Draw Rect with varying left side, equal to the item's right side
-//                // plus negative displacement dX
-//                c.drawRect((float) itemView.getRight() + dX, (float) itemView.getTop(),
-//                        (float) itemView.getRight(), (float) itemView.getBottom(), p);
-//
-//                //Set the image icon for Left swipe
-//                c.drawBitmap(icon,
-//                        (float) itemView.getRight() - convertDpToPx(16) - icon.getWidth(),
-//                        (float) itemView.getTop() + ((float) itemView.getBottom() - (float) itemView.getTop() - icon.getHeight()) / 2, p);
-//            }
-//
-//            // Fade out the view as it is swiped out of the parent's bounds
-//            final float alpha = ALPHA_FULL - Math.abs(dX) / (float) viewHolder.itemView.getWidth();
-//            viewHolder.itemView.setAlpha(alpha);
-//            viewHolder.itemView.setTranslationX(dX);
-//
-//        } else {
-//            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
-//        }
-//    }
+    @Override
+    public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        Bitmap icon;
+        if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
 
-//    private int convertDpToPx(int dp) {
-//        return Math.round(dp * (context.getResources().getDisplayMetrics().xdpi / DisplayMetrics.DENSITY_DEFAULT));
-//    }
+            View itemView = viewHolder.itemView;
+            float height = (float) itemView.getBottom() - (float) itemView.getTop();
+            float width = height / 3;
+
+            if (dX > 0) {
+                p.setColor(ContextCompat.getColor(context, R.color.secondaryColor));
+                RectF background = new RectF((float) itemView.getLeft(), (float) itemView.getTop(), dX, (float) itemView.getBottom());
+                c.drawRect(background, p);
+                icon = BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_edit_white);
+                RectF icon_dest = new RectF((float) itemView.getLeft() + width, (float) itemView.getTop() + width, (float) itemView.getLeft() + 2 * width, (float) itemView.getBottom() - width);
+                c.drawBitmap(icon, null, icon_dest, p);
+            } else {
+                p.setColor(Color.parseColor("#D32F2F"));
+                RectF background = new RectF((float) itemView.getRight() + dX, (float) itemView.getTop(), (float) itemView.getRight(), (float) itemView.getBottom());
+                c.drawRect(background, p);
+                icon = BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_delete_white);
+                RectF icon_dest = new RectF((float) itemView.getRight() - 2 * width, (float) itemView.getTop() + width, (float) itemView.getRight() - width, (float) itemView.getBottom() - width);
+                c.drawBitmap(icon, null, icon_dest, p);
+            }
+        }
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+    }
 }
+
--- app/src/main/java/learning/memento/toto/my_memonto/MainActivity.java
+++ app/src/main/java/learning/memento/toto/my_memonto/MainActivity.java
@@ -1,5 +1,8 @@
 package learning.memento.toto.my_memonto;
 
+import android.content.DialogInterface;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -8,6 +11,8 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -20,9 +25,15 @@ import data.Category;
 
 public class MainActivity extends AppCompatActivity {
 
-    RecyclerView recyclerView;
-    CategoriesAdapter mCategoriesAdapter;
-    List<Category> categoryList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private CategoriesAdapter mCategoriesAdapter;
+    private List<Category> categoryList = new ArrayList<>();
+    private FloatingActionButton fab;
+    private EditText addCategoryEditText;
+    private AlertDialog.Builder alertDialog;
+    private View view;
+    private boolean add;
+    private int edit_position;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,21 +41,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         recyclerView = (RecyclerView) findViewById(R.id.categories_rv);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
 
         mCategoriesAdapter = new CategoriesAdapter(this, categoryList);
         recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false));
         recyclerView.setAdapter(mCategoriesAdapter);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
-//        recyclerView.addItemDecoration(new MyDividerItemDecoration(this, LinearLayoutManager.VERTICAL, 16));
+        recyclerView.addItemDecoration(new MyDividerItemDecoration(this, LinearLayoutManager.VERTICAL, 16));
 //        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
 
         //add delete functionality to the recyclerview by swipe it left
-        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new SimpleCallbackTouchHelper(0, ItemTouchHelper.RIGHT) {
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new SimpleCallbackTouchHelper(MainActivity.this, 0, ItemTouchHelper.RIGHT | ItemTouchHelper.LEFT) {
             @Override
             public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
                 int position = viewHolder.getAdapterPosition();
-                categoryList.remove(position);
-                mCategoriesAdapter.notifyDataSetChanged();
+                if (direction == ItemTouchHelper.LEFT) {
+                    categoryList.remove(position);
+                    mCategoriesAdapter.notifyItemRemoved(position);
+                    mCategoriesAdapter.notifyItemRangeChanged(position, categoryList.size());
+                } else {
+//                    Toast.makeText(MainActivity.this, "Swipped Right", Toast.LENGTH_SHORT).show();
+                    removeView();
+                    edit_position = position;
+                    alertDialog.setTitle("Edit Country");
+                    addCategoryEditText.setText(categoryList.get(position).getCategoryName());
+                    alertDialog.show();
+                }
+
+
             }
         });
         itemTouchHelper.attachToRecyclerView(recyclerView);
@@ -61,10 +85,22 @@ public class MainActivity extends AppCompatActivity {
             }
         }));
 
-        addCategoryItems();
+//        addCategoryItems();
+
+        //add onclicklistener to the fab button
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                removeView();
+                add = true;
+                alertDialog.setTitle("Add Country");
+                addCategoryEditText.setText("");
+                alertDialog.show();
+            }
+        });
     }
 
-    private void addCategoryItems(){
+    private void addCategoryItems() {
         Category c1 = new Category("new category 1", 2, "22 Dec, 2018");
         Category c2 = new Category("new category 2", 1, "02 Jan, 2018");
         Category c3 = new Category("new category 3", 0, "15 Mar, 2018");
@@ -74,5 +110,34 @@ public class MainActivity extends AppCompatActivity {
         categoryList.add(c3);
 
         mCategoriesAdapter.notifyDataSetChanged();
+    }
+
+    //action of the Floating Action Button
+    public void addNewCategory(String categoryName) {
+        Category cat = new Category(categoryName, (int) (Math.random() * 20), "23, May 2018");
+        categoryList.add(0, cat);
+        mCategoriesAdapter.notifyItemInserted(0);
+        recyclerView.smoothScrollToPosition(0);
+//        mCategoriesAdapter.notifyDataSetChanged();
+    }
+
+    private void initDialog() {
+        alertDialog = new AlertDialog.Builder(this);
+        view = getLayoutInflater().inflate(R.layout.add_category_dialog, null);
+        alertDialog.setView(view);
+        alertDialog.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                addNewCategory(addCategoryEditText.getText().toString());
+                dialog.dismiss();
+            }
+        });
+        addCategoryEditText = (EditText) view.findViewById(R.id.add_category_et);
+    }
+
+    private void removeView(){
+        if(view.getParent()!=null) {
+            ((ViewGroup) view.getParent()).removeView(view);
+        }
     }
 }
